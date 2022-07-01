cacdfd0b90450e991a532d3912db21b774c455fe
Muxriddin Shamsiboyev <muxriddin.shamsiboyev1998@gmail.com>
2022-07-01 20:58:07 +0500

    Navbar Categories add

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,257 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <div class="container-fluid">
+
+            <!-- header qatori start -->
+            <div class="row">
+                <div class="col">
+                    <header>
+                        <NavBar />
+                    </header>
+                </div>
+            </div>
+            <!-- header qatori end -->
+
+            <!-- main qatori start -->
+            <div class="row mt-4">
+
+                <!-- sideBar start -->
+                <div class="col-12 col-md-2">
+                    <Categories />
+                </div>
+                <!-- sideBar end -->
+
+                <!-- kontent start -->
+                <div class="col-12 col-md-10">
+
+                    <!-- kitoblar qatori start -->
+                    <div class="row mt-4 mt-md-0">
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="../public/img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="book.html" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" id="akmal" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+                        <!-- kitob start -->
+                        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="img/otkan-kunlar.jpg" class="card-img-top" alt="kitob rasmi">
+                                <div class="card-body">
+                                    <h5 class="card-title">O'tkan kunlar</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the
+                                        card's content.</p>
+                                    <a href="#" class="btn btn-primary">O'qish</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- kitob end -->
+
+
+
+                    </div>
+                    <!-- kitoblar qatori end -->
+
+                    <!-- pagination start -->
+                    <div class="row">
+                        <div class="col">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination">
+                                    <li class="page-item">
+                                        <a class="page-link" href="#" aria-label="Previous">
+                                            <span aria-hidden="true">&laquo;</span>
+                                        </a>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#" aria-label="Next">
+                                            <span aria-hidden="true">&raquo;</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <!-- pagination end -->
+
+                </div>
+                <!-- kontent end -->
+
+            </div>
+            <!-- main qatori end -->
+
+
+            <!-- footer start -->
+            <div class="row">
+                <div class="col bg-dark text-light p-5">
+                    <footer>
+                        &copy; All rights reserved
+                    </footer>
+                </div>
+            </div>
+            <!-- footer end -->
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+import NavBar from "@/components/NavBar";
+import Categories from "@/components/Categories";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Categories,
+      NavBar
+
   }
 }
 </script>
---
+++ src/components/Categories.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul class="list-group">
+        <li class="list-group-item">Detektiv</li>
+        <li class="list-group-item active" aria-current="true">Klassika</li>
+        <li class="list-group-item">Fantastika</li>
+        <li class="list-group-item">Komediya</li>
+        <li class="list-group-item">Romantika</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "Categories"
+}
+</script>
+
+<style scoped>
+
+</style>
